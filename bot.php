ae0d4ca294e5b615c780e3135444c207b1a64222
“Wasin <wasin.r@halalize.com>
2017-09-10 17:32:34 +0700

    new order

--- bot.php
+++ bot.php
@@ -19,14 +19,14 @@ if (!is_null($events['events'])) {
 			// Build message to reply back
 			$messages = [
 				'type' => 'text',
-				'text' => $text
+				'text' => 'มีออเดอร์มาใหม่ครับ'
 			];
 
 			// Make a POST Request to Messaging API to reply to sender
 			$url = 'https://api.line.me/v2/bot/message/reply';
 			$data = [
 				'replyToken' => $replyToken,
-				'messages' => ['สวัสดีครับ'],
+				'messages' => [$messages],
 			];
 			$post = json_encode($data);
 			$headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
